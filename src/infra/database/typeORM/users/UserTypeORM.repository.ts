7742d865a0b0fc9f6e2965b4104fc2417dd0a3f3
João Victor Rodrigues <jvpr123@outlook.com>
2022-08-24 12:22:14 -0300

    feat: :sparkles: implement FindUserRepository interface inside UserTypeOrm repository

--- src/infra/database/typeORM/users/UserTypeORM.repository.ts
+++ src/infra/database/typeORM/users/UserTypeORM.repository.ts
@@ -1,15 +1,22 @@
 import { IUserModel } from "src/domain/models/User.model";
 import { ICreateUserDto } from "src/domain/dto/users/CreateUser.dto";
-import { ICreateUserRepository } from "src/data/protocols/CreateUserRepository.interface";
+import { ICreateUserRepository } from "src/data/protocols/database/CreateUserRepository.interface";
 
 import { Repository } from "typeorm";
 import { User } from "./User.entity";
+import { IFindUserRepository } from "src/data/protocols/database/FindUserRepository.interface";
 
-export class UserTypeOrmRepository implements ICreateUserRepository {
+export class UserTypeOrmRepository
+  implements ICreateUserRepository, IFindUserRepository
+{
   constructor(private repository: Repository<User>) {}
 
   async create(data: ICreateUserDto): Promise<Omit<IUserModel, "password">> {
     const userToCreate = this.repository.create(data);
     return await this.repository.save(userToCreate);
+  }
+
+  async findByEmail(email: string): Promise<Omit<IUserModel, "password">> {
+    return await this.repository.findOneBy({ email });
   }
 }
