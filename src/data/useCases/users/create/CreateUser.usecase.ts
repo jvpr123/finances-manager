d1a42be81ef8b98463845eac0caaf846852b8846
João Victor Rodrigues <jvpr123@outlook.com>
2022-08-24 15:26:10 -0300

    feat: :sparkles: add verification to createUser use-case to throw error when email provided already exists

--- src/data/useCases/users/create/CreateUser.usecase.ts
+++ src/data/useCases/users/create/CreateUser.usecase.ts
@@ -6,22 +6,24 @@ import { IEncrypter } from "src/data/protocols/cryptography/Encrypter.interface"
 import { ICreateUserRepository } from "src/data/protocols/database/CreateUserRepository.interface";
 
 import { ValidationError } from "src/errors/Validation.error";
+import { IFindUserRepository } from "src/data/protocols/database/FindUserRepository.interface";
 
 export class CreateUserUseCase implements ICreateUserUseCase {
   constructor(
     private readonly validator: IValidator,
     private readonly encrypter: IEncrypter,
-    private readonly repository: ICreateUserRepository
+    private readonly repository: ICreateUserRepository & IFindUserRepository
   ) {}
 
   async execute(input: any): Promise<Partial<IUserModel>> {
     const { isValid, data } = this.validator.validate(input);
+    const emailAlreadyExists = await this.repository.findByEmail(input?.email);
 
-    if (!isValid) {
-      throw new ValidationError(data);
-    }
+    if (!isValid) throw new ValidationError(data);
+    if (emailAlreadyExists) throw new ValidationError(["Email already in use"]);
 
     const hashedPassword = await this.encrypter.hash(data.password);
+
     return await this.repository.create({
       ...data,
       password: hashedPassword,
