4aed583732f151ee6dad9ba12b992136887e3c06
João Victor Rodrigues <jvpr123@outlook.com>
2022-08-25 11:35:58 -0300

    test: :test_tube: add integration tests for findAllUsers use-case routes

---
+++ src/__tests__/e2e/users/FindAllUsers.test.ts
@@ -0,0 +1,34 @@
+import request from "supertest";
+import { DataSource } from "typeorm";
+
+import { makeDataSource } from "src/__tests__/utils/typeORM/DataSource.factory";
+import { User } from "src/infra/database/typeORM/users/User.entity";
+import { makeFakeUserInput } from "src/__tests__/utils/UserMocks.factory";
+
+describe("Find All Users (GET /users)", () => {
+  const req = request("http://localhost:3030/dev/users");
+  let ds: DataSource;
+
+  beforeAll(async () => {
+    ds = await makeDataSource();
+    await req.post("/").send(makeFakeUserInput());
+  });
+
+  afterEach(async () => ds.getRepository<User>(User).clear());
+
+  it("should return 200 with user data when valid email is provided", async () => {
+    const response = await req.get("/");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("users");
+  });
+
+  it("should return 200 with empty array when no user is registered", async () => {
+    const response = await req.get("/");
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toStrictEqual({
+      users: [],
+    });
+  });
+});
