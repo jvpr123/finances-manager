abc6669c3777ce58e7fe1e03b8d96ee449d4cd6e
João Victor Rodrigues <jvpr123@outlook.com>
2022-08-29 16:57:04 -0300

    test: :test_tube: add integration tests for updateUnit use-case features

---
+++ src/__tests__/e2e/units/UpdateUnit.test.ts
@@ -0,0 +1,61 @@
+import request from "supertest";
+import { DataSource } from "typeorm";
+import { randomUUID } from "crypto";
+
+import { User } from "src/infra/database/typeORM/users/User.entity";
+
+import { makeDataSource } from "src/__tests__/utils/typeORM/DataSource.factory";
+import {
+  makeFakeUpdateUnitInput,
+  makeFakeCreateUnitDto,
+} from "src/__tests__/utils/UnitMocks.factory";
+
+describe("Update Unit (PATCH /units)", () => {
+  const req = request("http://localhost:3030/dev/units");
+
+  const createUnitInput = makeFakeCreateUnitDto();
+  const updateUnitInput = makeFakeUpdateUnitInput();
+
+  let ds: DataSource;
+
+  beforeAll(async () => {
+    ds = await makeDataSource();
+
+    const { body } = await req.post("/").send(createUnitInput);
+    updateUnitInput.id = body.unit.id;
+  });
+
+  afterAll(async () => ds.getRepository<User>(User).clear());
+
+  it("should return 200 with updated unit data when valid data is provided", async () => {
+    const response = await req.patch("/").send(updateUnitInput);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveProperty("updated");
+    expect(response.body).toHaveProperty("unit");
+  });
+
+  it("should return 400 with errors description when invalid data is provided", async () => {
+    const response = await req
+      .patch("/")
+      .send({ ...updateUnitInput, name: "" });
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toStrictEqual({
+      message: "Request contains invalid data",
+      errors: ['"name" is not allowed to be empty'],
+    });
+  });
+
+  it("should return 404 with errors description when unit is not found", async () => {
+    const id = randomUUID();
+    const response = await req
+      .patch("/")
+      .send({ ...updateUnitInput, id, name: "new_name" });
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toStrictEqual({
+      message: `Could not update: data related to ID ${id} not found`,
+    });
+  });
+});
