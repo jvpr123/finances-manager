265d6418170b8188a80f9ae10804b7168ec0898f
João Victor Rodrigues <jvpr123@outlook.com>
2022-09-13 10:07:34 -0300

    feat: :sparkles: update createCategory use-case to verify if title provided already exists

--- src/data/useCases/categories/create/CreateCategory.usecase.ts
+++ src/data/useCases/categories/create/CreateCategory.usecase.ts
@@ -4,19 +4,28 @@ import { ICategoryModel } from "src/domain/models/Category.model";
 
 import { IValidator } from "src/data/protocols/validation/Validator.interface";
 import { ICreateCategoryRepository } from "src/data/protocols/database/categories/CreateCategoryRepository.interface";
+import { IFindCategoryRepository } from "src/data/protocols/database/categories/FindCategoryRepository.interface";
 
 import { ValidationError } from "src/errors/Validation.error";
+
+type IRepository = ICreateCategoryRepository & IFindCategoryRepository;
 
 export class CreateCategoryUseCase implements ICreateCategoryUseCase {
   constructor(
     private readonly validator: IValidator,
-    private readonly repository: ICreateCategoryRepository
+    private readonly repository: IRepository
   ) {}
 
   async execute(input: ICreateCategoryInput): Promise<ICategoryModel> {
     const { isValid, data } = this.validator.validate(input);
+    const categoryTitleAlreadyExists = await this.repository.findByTitle(
+      input?.title
+    );
 
     if (!isValid) throw new ValidationError(data);
+    if (categoryTitleAlreadyExists) {
+      throw new ValidationError(["Category title already in use"]);
+    }
 
     return await this.repository.create(data);
   }
